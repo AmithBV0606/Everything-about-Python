8a2663149d404029306221bf14fbed5adf5ad3ef
Amith B V <amithrao0606@gmail.com>
2025-08-04 12:57:44 +0530

    Lesson-3 Statements and Ternary Operator

---
+++ LESSON-03/02-Statements.py
@@ -0,0 +1,10 @@
+number = 455
+print('')
+
+if number > 50:
+    print('Number is greater than 50!!')
+else:
+    print('Number is NOT greater than 50!!')
+
+# Ternary Operator
+print('Ternary: Number is greater than 50!!') if number > 50 else print('Ternary: Number is NOT greater than 50!!')
