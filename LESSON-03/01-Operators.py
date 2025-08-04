4f818a42027c8921bba2a57b960e2a3c187090df
Amith B V <amithrao0606@gmail.com>
2025-08-04 12:36:22 +0530

    Lesson-2 Operators

---
+++ LESSON-03/01-Operators.py
@@ -0,0 +1,82 @@
+# Operators 
+
+# 1) Assignment Operator :
+
+name = 'Amith'
+print(name)
+
+number = 25
+print(number)
+
+# 2) Arithmetic Operators : 
+
+sum = 2 + 5
+print(sum)
+
+diff = 5 - 2
+print(diff)
+
+mul = 5 * 2
+print(mul)
+
+div = 5 / 2
+print(div)
+
+# Floor division : div = 5 // 2, round(24 / 5)
+
+mod = 5 % 2 # Remainder
+print(mod)
+
+# Power 
+power = 2 ** 3
+print(power)
+
+# 3) Increment and Decrement :
+meaning = 40
+
+meaning += 1
+print(meaning)
+
+meaning -= 2
+print(meaning)
+
+meaning *= 2
+print(meaning)
+
+meaning /= 2
+print(meaning)
+
+# 4) Concatination : 
+
+firstName = 'Amith'
+lastName = 'Rao'
+print(firstName + ' ' + lastName)
+
+# 5) Comparisson Operator : 
+
+a = 10
+b = 20
+
+print(a == b)
+
+print(a != b)
+
+print(a > b)
+print(a >= b)
+
+print(a < b)
+print(a <= b)
+
+# 6) Boolean Operator : 
+x = True
+y = False
+z = True
+
+print(not x)
+print(not y)
+print(not z)
+
+print(x and y)
+
+print(x or y)
+print(y or z)
