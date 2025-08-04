6620f0c4a386b2ce6e45d3073120af0f54eaabd7
Amith B V <amithrao0606@gmail.com>
2025-08-04 12:07:48 +0530

    Lesson-2 Basic python syntax

---
+++ LESSON-02/01-Basic-Syntax.py
@@ -0,0 +1,26 @@
+# Variables
+
+# What cannot be used(basic naming convention) :
+
+# Numbers before the variables name : 001name = "Amith"
+# Special characters(Except underscore) before or after the variables name : name! = "Amith"
+# "-" also cannot be used : first-name = "Amith"
+
+# Reserved keywords
+
+# if, else, for
+
+line01 = "********************"
+line02 = "*                  *"
+line03 = "*     WELCOME!     *"
+
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+
+# Comment : Lines ignored by the compiler/interpretor
+
+# Indentation : Python does not ignore white spaces
